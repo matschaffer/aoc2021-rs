52135b453657acf908d318514198a126435c955c
Mat Schaffer <mat@elastic.co>
2021-12-11 08:42:13 +0900

    Day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -4,6 +4,10 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 #[structopt(name = "day2", about = "Solver for Advent of Code 2021 day 2")]
 struct Opt {
+    /// Aimed mode
+    #[structopt(short, long)]
+    aimed: bool,
+
     /// Filename
     #[structopt(name = "FILE")]
     filename: String,
@@ -14,27 +18,71 @@ struct Position {
     y: isize,
 }
 
+struct Sub {
+    x: isize,
+    depth: isize,
+    aim: isize,
+}
+
+impl Sub {
+    fn multiple(&self) -> isize {
+        self.x * self.depth
+    }
+}
+
 fn main() {
     let opt = Opt::from_args();
     let filename = opt.filename;
 
     let data = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    let data = split_data(&data);
-    let moves: Vec<Move> = data.iter().map(|i| parse_instruction(i)).collect();
+    let moves = parse_moves(&data);
 
-    let mut position = Position { x: 0, y: 0 };
+    if opt.aimed {
+        let mut sub = Sub {
+            x: 0,
+            depth: 0,
+            aim: 0,
+        };
+        sub_move(&mut sub, &moves);
 
-    move_robot(&mut position, &moves);
+        println!("Aimed final position: {}", sub.multiple());
+    } else {
+        let mut position = Position { x: 0, y: 0 };
+        direct_move(&mut position, &moves);
 
-    println!(
-        "Position x:{}, y:{} - multiple:{}",
-        position.x,
-        position.y,
-        position.x * position.y
-    );
+        println!(
+            "Position x:{}, y:{} - multiple:{}",
+            position.x,
+            position.y,
+            position.x * position.y
+        );
+    }
 }
 
-fn move_robot(position: &mut Position, moves: &[Move]) {
+fn parse_moves(data: &str) -> Vec<Move> {
+    let data = split_data(data);
+    let moves: Vec<Move> = data.iter().map(|i| parse_instruction(i)).collect();
+    moves
+}
+
+fn sub_move(sub: &mut Sub, moves: &[Move]) {
+    for m in moves {
+        match m {
+            Move::Up(n) => {
+                sub.aim -= n;
+            }
+            Move::Down(n) => {
+                sub.aim += n;
+            }
+            Move::Forward(n) => {
+                sub.x += n;
+                sub.depth += sub.aim * n;
+            }
+        }
+    }
+}
+
+fn direct_move(position: &mut Position, moves: &[Move]) {
     for m in moves {
         match m {
             Move::Forward(n) => {
@@ -94,13 +142,26 @@ mod tests {
     }
 
     #[test]
-    fn moving() {
+    fn direct_motion() {
         let mut position = Position { x: 0, y: 0 };
-        move_robot(
+        direct_move(
             &mut position,
             &[Move::Forward(5), Move::Down(5), Move::Up(1)],
         );
         assert_eq!(position.x, 5);
         assert_eq!(position.y, 4);
+    }
+
+    #[test]
+    fn aimed_motion() {
+        let data = include_str!("../data/intro.txt");
+        let moves = parse_moves(data);
+        let mut sub = Sub {
+            x: 0,
+            depth: 0,
+            aim: 0,
+        };
+        sub_move(&mut sub, &moves);
+        assert_eq!(sub.multiple(), 900);
     }
 }
